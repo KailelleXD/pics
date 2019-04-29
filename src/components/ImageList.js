abec2a9f9e2673fe6df73fdeff559dcf5532f711
Steven Ng <40282847+MrStevenNg@users.noreply.github.com>
2019-04-28 18:41:57 -0700

    displaying images through .map

---
+++ src/components/ImageList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ImageList.css';
+
+const ImageList = (props) => {
+    const images = props.images.map(({ alt_description, id, urls }) => {
+        return <img key={id} src={urls.regular} alt={alt_description} />
+    });
+
+    return <div>{images}</div>
+};
+
+export default ImageList;
